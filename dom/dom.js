f5d980ceee4e5221fabbe17d679a4437827b58b5
RM <rajlaxmi79812@gmail.com>
2024-12-31 14:30:24 +0530

    html change dome

--- dom/dom.js
+++ dom/dom.js
@@ -1,10 +1,20 @@
 // dom 
+// 4 pillar
+//  html  change (done )
+// selection on a element (done)
+// css change
+// event   listener (done)
+
 // event
 const story = document.body.querySelector(".story");
 // ye line hum textarea ke class ko access krne ke liya use kr rhai hai 
 
 
+  const htage = document.querySelector('h1')
+  // const p = document.querySelectorAll('p')
+  // console.log(p)
 
+  console.log(htage)
 const setText = document.body.querySelector("#set-text");
 // ye line hum textarea ke id ko access krne ke liya use kr rhai hai 
 

4b96dc6f880221f14d53f210104d43153fa252b5
RM <rajlaxmi79812@gmail.com>
2025-01-08 14:02:40 +0530

    parent, child

---
+++ secdom/script.js
@@ -0,0 +1,29 @@
+const parent = document.body.querySelector(".parent");
+
+const addChild = document.body.querySelector("#add-child");
+addChild.addEventListener("click",() =>{
+  // Only add a child if we don't already have one
+  // in addition to the text node "parent"
+  if (parent.childNodes.length > 1) {
+    return;
+  }
+
+  const child = document.createElement("div");
+  child.classList.add("child");
+  child.textContent = "child  h ye";
+//   add 
+  parent.appendChild(child);
+
+
+}
+);
+
+const removeChild = document.body.querySelector("#remove-child");
+removeChild.addEventListener("click", () =>{
+
+  const child = document.body.querySelector(".child");
+//   remove 
+  parent.removeChild(child);
+
+}
+);
